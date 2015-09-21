902813500177119f00dfb3fa1f3377750c714709
Robert Johnson <r.johnson@bom.gov.au>
2015-09-21 12:06:25 +1000

    some leaflet notes?

--- leaflet_and_shiny.R
+++ leaflet_and_shiny.R
@@ -11,16 +11,16 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  
+
 #   points <- eventReactive(input$recalc, {
 #     cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
 #   }, ignoreNULL = FALSE)
-  
+
   output$mymap <- renderLeaflet({
     leaflet() %>% addTiles() %>% setView(150, -20, zoom = 5) %>%
       addWMSTiles(
         "http://ereeftds.bom.gov.au/ereefs/tds/wms/ereefs/mwq_gridAgg_P1D?request=GetMap&service=WMS&version=1.3.0&time=2015-08-02T04:15:09.000Z&COLORSCALERANGE=0.1,10.0&BELOWMINCOLOR=transparent&ABOVEMAXCOLOR=extend",
-        #"http://ereeftds.bom.gov.au/ereefs/tds/wms/ereefs/mwq_gridAgg_P1D?request=GetMap&service=WMS&version=1.3.0&time=2015-07-30Z&COLORSCALERANGE=0.1,10.0&BELOWMINCOLOR=transparent&ABOVEMAXCOLOR=extend&CRSEPSG:4326", 
+        #"http://ereeftds.bom.gov.au/ereefs/tds/wms/ereefs/mwq_gridAgg_P1D?request=GetMap&service=WMS&version=1.3.0&time=2015-07-30Z&COLORSCALERANGE=0.1,10.0&BELOWMINCOLOR=transparent&ABOVEMAXCOLOR=extend&CRSEPSG:4326",
         layers = "Chl_MIM",
         options = WMSTileOptions(format = "image/png", transparent = TRUE),
         #options = WMSTileOptions(format = "application/vnd.google-earth.kmz"),
